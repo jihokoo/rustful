179ff5261a2ffbec89a251b17c16db9e00900dae
Travis <travis@travis-ci.org>
2015-01-06 07:11:45 +0000

    Update docs from Travis build 113

--- doc/implementors/core/cmp/trait.Ord.js
+++ doc/implementors/core/cmp/trait.Ord.js
@@ -1,7 +1,5 @@
 (function() {var implementors = {};
 implementors['mucell'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='mucell/struct.MuCell.html' title='mucell::MuCell'>MuCell</a>&lt;T&gt;",];
-implementors['time'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='time/struct.Timespec.html' title='time::Timespec'>Timespec</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='time/struct.Tm.html' title='time::Tm'>Tm</a>",];
-implementors['openssl'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='openssl/bn/struct.BigNum.html' title='openssl::bn::BigNum'>BigNum</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/search-index.js
+++ doc/search-index.js
@@ -1,3 +1,2 @@
 var searchIndex = {};
-searchIndex['rustful_macros'] = {"items":[[0,"","rustful_macros","This crate provides some helpful macros for rustful, including `insert_routes!` and `content_type!`."],[15,"content_type!","","A macro for assigning content types."],[15,"try_send!","","A macro for callig `send` in response and aborting the handle function if it fails."]],"paths":[]};
 initSearch(searchIndex);
