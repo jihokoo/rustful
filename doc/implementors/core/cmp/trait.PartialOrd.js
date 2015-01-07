614289254dba6599fa276d1ba3cb2769e817c802
Travis <travis@travis-ci.org>
2015-01-07 06:58:04 +0000

    Update docs from Travis build 113

--- doc/implementors/core/cmp/trait.PartialOrd.js
+++ doc/implementors/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors['mucell'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='mucell/struct.MuCell.html' title='mucell::MuCell'>MuCell</a>&lt;T&gt;",];
 implementors['rustc-serialize'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='enum' href='rustc-serialize/json/enum.Json.html' title='rustc-serialize::json::Json'>Json</a>",];
 
             if (window.register_implementors) {
