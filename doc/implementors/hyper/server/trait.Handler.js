0491dee97fae44899a4d295e479d2ae25020f5b3
Travis <travis@travis-ci.org>
2015-01-11 15:09:02 +0000

    Update docs from Travis build 115

---
+++ doc/implementors/hyper/server/trait.Handler.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['hyper'] = [];
+implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;R, H, C&gt; <a class='trait' href='hyper/server/trait.Handler.html' title='hyper::server::Handler'>HyperHandler</a> for <a class='struct' href='rustful/struct.ServerInstance.html' title='rustful::ServerInstance'>ServerInstance</a>&lt;R, C&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- doc/implementors/unsafe-any/trait.UnsafeAnyExt.js
+++ doc/implementors/unsafe-any/trait.UnsafeAnyExt.js
@@ -1,5 +1,7 @@
 (function() {var implementors = {};
 implementors['unsafe-any'] = [];
+implementors['hyper'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='unsafe-any/trait.UnsafeAnyExt.html' title='unsafe-any::UnsafeAnyExt'>UnsafeAnyExt</a> for <a class='trait' href='hyper/header/trait.HeaderFormat.html' title='hyper::header::HeaderFormat'>HeaderFormat</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='unsafe-any/trait.UnsafeAnyExt.html' title='unsafe-any::UnsafeAnyExt'>UnsafeAnyExt</a> for <a class='trait' href='hyper/net/trait.NetworkStream.html' title='hyper::net::NetworkStream'>NetworkStream</a>",];
+implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='unsafe-any/trait.UnsafeAnyExt.html' title='unsafe-any::UnsafeAnyExt'>UnsafeAnyExt</a> for <a class='trait' href='rustful/header/trait.HeaderFormat.html' title='rustful::header::HeaderFormat'>HeaderFormat</a> + 'static","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='unsafe-any/trait.UnsafeAnyExt.html' title='unsafe-any::UnsafeAnyExt'>UnsafeAnyExt</a> for <a class='trait' href='hyper/net/trait.NetworkStream.html' title='hyper::net::NetworkStream'>NetworkStream</a> + 'static",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
